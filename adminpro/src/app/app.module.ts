dcfe3dfbd905e0615da31776662c0656df2ea7ba
cjgmj <cjgmj@hotmail.com>
2018-12-14 23:03:25 +0100

    Componentes y servicios iniciales

--- adminpro/src/app/app.module.ts
+++ adminpro/src/app/app.module.ts
@@ -1,11 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    ProgressComponent,
+    Graficas1Component,
+    NopagefoundComponent,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent
   ],
   imports: [
     BrowserModule

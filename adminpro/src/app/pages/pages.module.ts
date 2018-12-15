3573a22c44923edbc7b5a8524076dc10fea1541d
cjgmj <cjgmj@hotmail.com>
2018-12-15 12:26:48 +0100

    Rutas hijas

--- adminpro/src/app/pages/pages.module.ts
+++ adminpro/src/app/pages/pages.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
+
+import { SharedModule } from '../shared/shared.module';
+
+import { PAGES_ROUTES } from './pages.routes';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
 import { PagesComponent } from './pages.component';
-
-import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
     declarations: [
@@ -18,7 +21,8 @@ import { SharedModule } from '../shared/shared.module';
         Graficas1Component
     ],
     imports: [
-        SharedModule
+        SharedModule,
+        PAGES_ROUTES
     ]
 })
 export class PagesModule { }

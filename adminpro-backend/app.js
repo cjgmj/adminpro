acf533c2cd4640861fbea4532ee4a356e0f2c1d9
cjgmj <cjgmj@hotmail.com>
2018-12-20 21:51:01 +0100

    Conexión con mongoose

--- adminpro-backend/app.js
+++ adminpro-backend/app.js
@@ -1,8 +1,15 @@
 // Requires
 var express = require('express');
+var mongoose = require('mongoose');
 
 // Inicializar variables
 var app = express();
+
+// Conexión a la base de datos
+mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (error, response) => {
+    if (error) throw error;
+    console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online');
+});
 
 // Rutas
 app.get('/', (request, response, next) => {

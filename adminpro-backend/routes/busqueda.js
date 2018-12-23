a04657a1968ce536142bd4b2db3008986468427b
cjgmj <cjgmj@hotmail.com>
2018-12-23 14:34:48 +0100

    Búsqueda por colección

--- adminpro-backend/routes/busqueda.js
+++ adminpro-backend/routes/busqueda.js
@@ -4,6 +4,43 @@ var Hospital = require('../models/hospital');
 var Medico = require('../models/Medico');
 var Usuario = require('../models/usuario');
 
+// ===============================
+// Búsqueda colección
+// ===============================
+app.get('/coleccion/:tabla/:busqueda', (req, res) => {
+    var tabla = req.params.tabla;
+    var regex = new RegExp(req.params.busqueda, 'i');
+    var promesa;
+
+    switch (tabla) {
+        case 'usuarios':
+            promesa = buscarUsuarios(regex);
+            break;
+        case 'medicos':
+            promesa = buscarMedicos(regex);
+            break;
+        case 'hospitales':
+            promesa = buscarHospitales(regex);
+            break;
+        default:
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'Colección no encontrada',
+                error: { message: 'Colección no encontrada' }
+            });
+    }
+
+    promesa.then(data => {
+        res.status(200).json({
+            ok: true,
+            [tabla]: data
+        });
+    })
+});
+
+// ===============================
+// Búsqueda simúltanea
+// ===============================
 app.get('/todo/:busqueda', (req, res) => {
     var regex = new RegExp(req.params.busqueda, 'i');
 

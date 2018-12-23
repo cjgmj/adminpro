715e85f75b9c41b4f3f963a6e4bcaf1394a88b31
cjgmj <cjgmj@hotmail.com>
2018-12-23 16:15:29 +0100

    Desplegar imágenes - FileSystem

--- adminpro-backend/app.js
+++ adminpro-backend/app.js
@@ -25,6 +25,11 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', { useNewUrlP
     console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online');
 });
 
+// Server index config
+var serveIndex = require('serve-index');
+app.use(express.static(__dirname + '/'));
+app.use('/uploads', serveIndex(__dirname + '/uploads'));
+
 // Rutas
 app.use('/usuario', usuarioRoutes);
 app.use('/hospital', hospitalRoutes);

e745de2d3b7cb38fd88724aea15892b9af9779d6
cjgmj <cjgmj@hotmail.com>
2018-12-24 13:48:36 +0100

    Cambio CORS

--- adminpro-backend/app.js
+++ adminpro-backend/app.js
@@ -2,17 +2,15 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 // Inicializar variables
 var app = express();
 
 // CORS
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
-    next();
-});
+app.use(cors({
+    origin: 'http://localhost:4200'
+}));
 
 // Body Parser
 app.use(bodyParser.urlencoded({ extended: false }));
